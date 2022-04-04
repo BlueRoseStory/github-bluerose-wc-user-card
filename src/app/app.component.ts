88464454746753e68b0c03aa06b3a42da0ed3cd5
BlueRoseStory <scottb@tsbrooks.com>
2022-04-04 08:27:48 -0400

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   user1 = 'John Doe';
   user2 = 'Jane Doe';
-  maxCharText = 'Type here.';
+  maxCharText = 'Type here';
   token =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
 }

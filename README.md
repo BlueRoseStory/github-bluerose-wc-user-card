61a9845aa63486b01b6d7e58caddc680e4a1aeb2
BlueRoseStory <scottb@tsbrooks.com>
2022-04-04 08:26:35 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# bluerose-npm-yap8qb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/bluerose-npm-yap8qb)
---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  user1 = 'John Doe';
+  user2 = 'Jane Doe';
+  maxCharText = 'Type here.';
+  token =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+}
